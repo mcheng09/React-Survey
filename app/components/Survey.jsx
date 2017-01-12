43821d2035f458f9af5d7ba84a932ba6cda99d23
Michael Cheng <mcheng.oc@gmail.com>
2017-01-11 18:39:17 -0800

    Added table and questions

--- app/components/Survey.jsx
+++ app/components/Survey.jsx
@@ -4,7 +4,39 @@ var Survey = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>Survey Component</h1>
+        <div className='row'>
+          <h1 className='text-center'>Random Survey</h1><hr/>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Questions</th>
+                <th>Fill In</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>How much do you like pizza?</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Which is your favorite breed of dogs?</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Do you know how to snowboard?</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>How much money do you spend a day?</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>What is your dream car?</td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     )
   }
