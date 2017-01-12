70342dc5039f48af9c7b160cded65becb46afcc0
Michael Cheng <mcheng.oc@gmail.com>
2017-01-11 16:47:08 -0800

    Fixed up server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+  else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-  console.log("Yo Port 3000")
-})
+app.listen(PORT, function(){
+  console.log("Hey there! Server is running on port " + PORT);
